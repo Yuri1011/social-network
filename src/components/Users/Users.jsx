8fb10237805d7e63dbbb6284b3a2b26be7f42b1a
Yuri <dog-pit10@yandex.ru>
2021-02-17 00:11:37 +0300

    add pagination in page Users.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,18 +4,39 @@ import axios from "axios";
 import avatar from '../../assets/images/avatar.jpg'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
             this.props.setUsers(response.data.items);
         });
     }
 
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+
     render() {
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
         return (
             <div>
                 <div>
-                    <button onClick={this.getUsers}>Get Users</button>
+                    {pages.map(p => {
+                        return <span className={this.props.currentPage === p && c.selectedPage}
+                                     onClick={() => {
+                                         this.onPageChanged(p)
+                                     }}>{p}</span>
+                    })}
+                </div>
+                <div>
+                    {/*<button onClick={this.getUsers}>Get Users</button>*/}
                 </div>
                 {this.props.users.map(u => <div className={c.container} key={u.id}>
                     <div>
