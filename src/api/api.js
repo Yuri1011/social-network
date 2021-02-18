73d3eff06681e7790331ba8d6abaa957ecfade1f
Yuri <dog-pit10@yandex.ru>
2021-02-18 07:21:03 +0300

    added file with functions API request.

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "830641ed-510b-42e3-bd30-9338086d8ee0"
+    }
+})
+
+export const usersAPI = {
+    getUsers(currentPage, pageSize) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data);
+    },
+    followUser(id) {
+        return instance.post(`follow/${id}`).then(response => response.data);
+    },
+    unfollowUser(id) {
+        return instance.delete(`follow/${id}`).then(response => response.data);
+    }
+}
